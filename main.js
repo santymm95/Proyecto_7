3bb2424c18febbc10c950b2df943846e5dc171ef
santymm95 <santymm95@gmail.com>
2023-04-27 16:28:04 -0500

    Versión 2

---
+++ main.js
@@ -0,0 +1,30 @@
+ let btnGris = document.getElementById("gris");
+ let btnAzul = document.getElementById("azul");
+ let btnRojo = document.getElementById("rojo");
+ let btnPlata = document.getElementById("plata");
+ let imgChange = document.getElementById("imgChange");
+ let imgChange1 = document.getElementById("imgChange1");
+
+btnGris.onclick = function() {
+    imgChange.src = src="./img/1-Boxer-ct-100-es-gris-grafito.png"
+};
+
+btnAzul.onclick = function() {
+    imgChange.src =src="./img/1-Boxer-ct-100-es-azul.png" 
+}
+
+btnRojo.onclick = function() {
+    imgChange.src = src="./img/1-Boxer-ct-100-es-rojo.png"
+}
+
+btnPlata.onclick = function() {
+    imgChange.src =  src="./img/1-Boxer-ct-100-es-gris.png"
+}
+
+
+/*---*/
+
+
+
+
+
